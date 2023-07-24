b23425be6086fd51a21e70364f00c8a1377ddf44
blkcor <1029129867@qq.com>
2023-07-24 10:24:33 +0800

    feat: navigation style(apply for mobile)

--- src/components/nav/Navigator.tsx
+++ src/components/nav/Navigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import NavBlock from './navblock/NavBlock';
 import LoginButton from './loginButton/LoginButton';
+import NavMenu from './navmenu/NavMenu';
 type NavigatorProps = {
 
 };
@@ -12,7 +13,7 @@ const Navigator: React.FC<NavigatorProps> = () => {
     <div className='flex flex-row bg-white min-h-17 w-full gap-5 '>
       <div className='flex flex-3/8 gap-5 items-center justify-center pl-1rem'>
         <Link
-          className='sm:flex flex-1/3 text-4xl text-black font-bold decoration-none items-center justify-center'
+          className='sm:flex text-center block flex-1/3 text-4xl text-black font-bold decoration-none items-center justify-center'
           to='/'
         >VALTEC</Link>
         <div className='sm:flex hidden flex-2/3 gap-5 h-full'>
@@ -20,8 +21,11 @@ const Navigator: React.FC<NavigatorProps> = () => {
           <NavBlock title='上传点位' name='discuss' actived={actived==="discuss"}/>
           <NavBlock title='资讯' name='news' actived={actived==="news"}/>
         </div>
+        <div className='sm:hidden mr-2'>
+          <NavMenu/>
+        </div >
       </div>
-      <div className='sm:flex flex-5/8 flex-row items-center justify-end pr-1rem'>
+      <div className='flex flex-5/8 flex-row items-center justify-end pr-1rem'>
           <LoginButton/>
       </div>
     </div>
--- src/components/nav/loginButton/LoginButton.tsx
+++ src/components/nav/loginButton/LoginButton.tsx
@@ -9,7 +9,8 @@ const LoginButton:React.FC<LoginButtonProps> = () => {
   return(
     <button
       className='
-      w-20 p-2 bg-blue-400 text-white border-none border-rounded-1 font-400 text-sm
+      w-20 p-2 bg-blue-400 text-white border-none 
+      border-rounded-1 font-400 text-sm
       hover:cursor-pointer hover:bg-blue-500 
       '
     >
